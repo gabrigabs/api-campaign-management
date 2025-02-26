b5e953c2b0bdd5670cecbbf589d6a42cfa9a9b4e
Gabriel <gabrielbrodrigues1@gmail.com>
2025-02-26 14:28:05 -0300

    feat: add base repository interface for CRUD operations

---
+++ src/commons/interfaces/base.repository.interface.ts
@@ -0,0 +1,7 @@
+export interface BaseRepositoryInterface<T, CreateDto, UpdateDto> {
+  create(data: CreateDto): Promise<T>;
+  findAll(filter?: any): Promise<T[]>;
+  findBy(params: Partial<T>): Promise<T | null>;
+  update(id: string, data: UpdateDto): Promise<T>;
+  delete(id: string): Promise<T>;
+}
