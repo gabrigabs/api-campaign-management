f15a4a5a49e643b71ae90c8cd3ecb45e62d7801f
Gabriel <gabrielbrodrigues1@gmail.com>
2025-02-26 22:00:39 -0300

    refactor: rename users table to User and update schema accordingly

--- prisma/migrations/20250227005914_/migration.sql
+++ prisma/migrations/20250227005914_/migration.sql
@@ -1,14 +1,13 @@
 -- CreateTable
-CREATE TABLE "users" (
+CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
