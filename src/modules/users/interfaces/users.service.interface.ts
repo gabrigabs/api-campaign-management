ad3a049ad06f501b737b2b4f4c9a5d9545b20458
Gabriel <gabrielbrodrigues1@gmail.com>
2025-02-28 22:51:32 -0300

    refactor: simplify UsersServiceInterface by omitting unnecessary methods

--- src/modules/users/interfaces/users.service.interface.ts
+++ src/modules/users/interfaces/users.service.interface.ts
@@ -1,7 +1,11 @@
 import { BaseServiceInterface } from '@commons/interfaces/base.service.interface';
 import { User } from '@prisma/client';
+
 import { CreateUserDto } from '../dtos/create-user.dto';
 import { UpdateUserDto } from '../dtos/update-user.dto';
 
 export interface UsersServiceInterface
-  extends BaseServiceInterface<User, CreateUserDto, UpdateUserDto> {}
+  extends Omit<
+    BaseServiceInterface<User, CreateUserDto, UpdateUserDto>,
+    'findAll' | 'delete' | 'update'
+  > {}
--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -7,7 +7,6 @@ import {
 } from '@nestjs/common';
 import { UsersServiceInterface } from '../interfaces/users.service.interface';
 import { CreateUserDto } from '../dtos/create-user.dto';
-import { UpdateUserDto } from '../dtos/update-user.dto';
 import { COMPANIES_SERVICE, USERS_REPOSITORY } from '@commons/consts/consts';
 import { UsersRepositoryInterface } from '../interfaces/users.repository.interface';
 import { User } from '@prisma/client';
@@ -36,21 +35,9 @@ export class UsersService implements UsersServiceInterface {
     return this.usersRepository.create({ ...data, password: hashedPassword });
   }
 
-  findAll(): Promise<User[]> {
-    this.logger.log('Fetching all users');
-    return this.usersRepository.findAll();
-  }
   findBy(params: Partial<User>): Promise<User | null> {
     this.logger.log(`Finding user by: ${JSON.stringify(params)}`);
     return this.usersRepository.findBy(params);
-  }
-  update(id: string, data: UpdateUserDto): Promise<User> {
-    this.logger.log(`Updating user with ID: ${id}`);
-    return this.usersRepository.update(id, data);
-  }
-  async delete(id: string): Promise<void> {
-    this.logger.log(`Deleting user with ID: ${id}`);
-    await this.usersRepository.delete(id);
   }
 
   private async verifyIfUserAlreadyExistsByEmail(email: string): Promise<void> {
