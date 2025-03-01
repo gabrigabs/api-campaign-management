14a575b752a07eb6cf25127bd69b97c96dd525b9
Gabriel <gabrielbrodrigues1@gmail.com>
2025-02-28 21:00:02 -0300

    feat: add global exception filter and error response DTO for improved error handling

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { GlocalExceptionsFilter } from '@commons/filters/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,7 @@ async function bootstrap() {
   );
 
   app.setGlobalPrefix('api');
+  app.useGlobalFilters(new GlocalExceptionsFilter());
 
   const config = new DocumentBuilder()
     .setTitle('Campaign Management API')
