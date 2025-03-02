d703847d0a6b8fda8ddc68d5e56bee16ef7cd256
Gabriel <gabrielbrodrigues1@gmail.com>
2025-03-02 20:17:21 -0300

    chore: add example environment configuration and remove unusedMongoose module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './infra/databases/prisma/prisma.module';
 import { RabbitMQModule } from './infra/messaging/rabbitmq/rabbitmq.module';
-import { MongooseModule } from '@nestjs/mongoose';
 
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
